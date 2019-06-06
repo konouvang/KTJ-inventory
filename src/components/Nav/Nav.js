b96e0042621dc1ed0f45d6d15798a45ca931c4e4
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-06-06 10:36:22 -0500

    got user name to show on dom

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,28 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 
-// import React, { Component } from 'react';
 
-
-
-const Nav = (props) => (
-  <div className="nav">
+class Nav extends Component {
+    render() {
+      return (
+        <div className="nav">
     <Link to="/home">
     <img src="/images/user-icon.PNG" className="imgicon" /> 
-      {/* {this.props.alluserReducer} */}
+      
     </Link>
+    <Link className ="nav-link">{this.props.user.username}</Link>
     <div className="nav-right">
-      <Link className="nav-link-home" to="/home">
+      <Link className="nav-link" to="/home">
         {/* Show this link if they are logged in or not,
         but call this link 'Home' if they are logged in,
         and call this link 'Login / Register' if they are not */}
-        {props.user.id ? 'Dash Board' : 'Login / Register'}
+        {this.props.user.id ? 'Dash Board' : 'Login / Register'}
       </Link>
       {/* Show the link to the info page and the logout button if the user is logged in */}
-      {props.user.id && (
+      {this.props.user.id && (
         <>
           <Link className="nav-link" to="/secrets">
             Secrets Page
@@ -45,7 +46,48 @@ const Nav = (props) => (
     </Link>
     </div>
   </div>
-);
+      )
+    }
+}
+
+// const Nav = (props) => (
+//   <div className="nav">
+//     <Link to="/home">
+//     <img src="/images/user-icon.PNG" className="imgicon" /> 
+//       {/* {this.props.alluserReducer.username} */}
+//     </Link>
+//     <div className="nav-right">
+//       <Link className="nav-link-home" to="/home">
+//         {/* Show this link if they are logged in or not,
+//         but call this link 'Home' if they are logged in,
+//         and call this link 'Login / Register' if they are not */}
+//         {props.user.id ? 'Dash Board' : 'Login / Register'}
+//       </Link>
+//       {/* Show the link to the info page and the logout button if the user is logged in */}
+//       {props.user.id && (
+//         <>
+//           <Link className="nav-link" to="/secrets">
+//             Secrets Page
+//           </Link>
+//           <Link className="nav-link" to="/allusers">
+//             All User
+//           </Link>
+//           <Link className="nav-link" to="/masterdata">
+//             Master Data
+//           </Link>
+//           <LogOutButton className="nav-link"/>
+//         </>
+//       )}
+//       {/* Always show this link since the about page is not protected */}
+//       <Link className="nav-link" to="/about">
+//         About
+//       </Link>
+//       <Link to="/home">
+//     <img src="/images/home-icon.PNG" className="imgicon" /> 
+//     </Link>
+//     </div>
+//   </div>
+// );
 
 // Instead of taking everything from state, we just want the user
 // object to determine if they are logged in
