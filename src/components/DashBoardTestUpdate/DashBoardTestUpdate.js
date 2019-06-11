3f4e29c65c8ab7d4faf9fb78abcedf224a9ffbcf
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-06-11 15:34:31 -0500

    frontend for update is okay, need to figure out how to get database to update

--- src/components/DashBoardTestUpdate/DashBoardTestUpdate.js
+++ src/components/DashBoardTestUpdate/DashBoardTestUpdate.js
@@ -10,7 +10,6 @@ import Grid from '@material-ui/core/Grid';
 class DashBoardUpdate extends Component {
   state = {
     inventoryIsEditable: false,
-    username: '',
 
     inventoryUpdate: {
       batch: '',
@@ -79,9 +78,25 @@ render() {
               <img src={`images/${inventory.photos}`} alt={inventory.id}/>
               <div className="dashboard">
                 {this.state.inventoryIsEditable ?
-                <input placeholder="batch" value={inventory.id} name="batch"onChange={this.changeHandle}/> :
+                <>
+                <input placeholder="id" value={inventory.id} name="id"onChange={this.changeHandle}/>
+                <input placeholder="batch" value={inventory.batch} name="batch"onChange={this.changeHandle}/>
+                <input placeholder="name" value={inventory.name} name="name"onChange={this.changeHandle}/>
+                <input placeholder="product_line" value={inventory.product_line} name="product_line"onChange={this.changeHandle}/>
+                <input placeholder="length" value={inventory.length} name="length"onChange={this.changeHandle}/>
+                <input placeholder="texture" value={inventory.texture} name="texture"onChange={this.changeHandle}/>
+                <input placeholder="color" value={inventory.color} name="color"onChange={this.changeHandle}/>
+                <input placeholder="hair_type" value={inventory.hair_type} name="hair_type"onChange={this.changeHandle}/>
+                <input placeholder="region_type" value={inventory.region_type} name="region_type"onChange={this.changeHandle}/>
+                <input placeholder="factory" value={inventory.factory} name="factory"onChange={this.changeHandle}/>
+                <input placeholder="current_location" value={inventory.current_location} name="current_location"onChange={this.changeHandle}/>
+                <input placeholder="quantity" value={inventory.quantity} name="quantity"onChange={this.changeHandle}/>
+                <input placeholder="cost_of_batch" value={inventory.cost_of_batch} name="cost_of_batch"onChange={this.changeHandle}/>
+                <input placeholder="price_per_unit" value={inventory.price_per_unit} name="price_per_unit"onChange={this.changeHandle}/>
+                <input placeholder="qr_code" value={inventory.qr_code} name="qr_code"onChange={this.changeHandle}/></>
+                :
+                <>
                 <p>{inventory.id}</p>
-                }
                 <p>{inventory.batch}</p>
                 <p>{inventory.name}</p>
                 <p>{inventory.product_line}</p>
@@ -95,33 +110,19 @@ render() {
                 <p>{inventory.quantity}</p>
                 <p>{inventory.cost_of_batch}</p>
                 <p>{inventory.price_per_unit}</p>
-                <p>{inventory.qr_code}</p>
+                <p>{inventory.qr_code}</p></>
+                }
+
                 <button onClick={this.removeItem(inventory.id)}>Remove</button>
               </div>
               {this.state.inventoryIsEditable ?
                 <div>
                   <form onSubmit={this.saveUpdate} key={index}>
-                    <input placeholder="batch" value={this.state.enteredbatch} name="batch"onChange={this.changeHandle}/>
-                    <input placeholder="name" value={this.state.enteredname} name="name"onChange={this.changeHandle}/>
-                    <input placeholder="product_line" value={this.state.enteredproduct_line} name="product_line"onChange={this.changeHandle}/>
-                    <input placeholder="length" value={this.state.enteredlength} name="length"onChange={this.changeHandle}/>
-                    <input placeholder="texture" value={this.state.enteredtexture} name="texture"onChange={this.changeHandle}/>
-                    <input placeholder="color" value={this.state.enteredcolor} name="color"onChange={this.changeHandle}/>
-                    <input placeholder="hair_type" value={this.state.enteredhair_type} name="hair_type"onChange={this.changeHandle}/>
-                    <input placeholder="region_type" value={this.state.enteredregion_type} name="region_type"onChange={this.changeHandle}/>
-                    <input placeholder="factory" value={this.state.enteredfactory} name="factory"onChange={this.changeHandle}/>
-                    <input placeholder="current_location" value={this.state.enteredcurrent_location} name="current_location"onChange={this.changeHandle}/>
-                    <input placeholder="quantity" value={this.state.enteredquantity} name="quantity"onChange={this.changeHandle}/>
-                    <input placeholder="cost_of_batch" value={this.state.enteredcost_of_batch} name="cost_of_batch"onChange={this.changeHandle}/>
-                    <input placeholder="price_per_unit" value={this.state.enteredprice_per_unit} name="price_per_unit"onChange={this.changeHandle}/>
-                    <input placeholder="photos" value={this.state.enteredphotos} name="photos"onChange={this.changeHandle}/>
-                    <input placeholder="qr_code" value={this.state.enteredqr_code} name="qr_code"onChange={this.changeHandle}/>
                     <button type="submit">Save</button>
                   </form>
                   <button onClick={this.updateItem(inventory.id)}>Update</button> 
                 </div> :
                 <button onClick={this.editInventory}>Edit Inventory</button>
-                
               }
       </Grid>
 
