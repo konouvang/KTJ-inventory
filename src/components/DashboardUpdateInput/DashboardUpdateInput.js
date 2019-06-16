9dd58bd67ed49c56fcb68cf5dd1ea541f4254877
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-06-16 10:25:13 -0500

    updated the type fields in input so that text is text and numbers is numbers

--- src/components/DashboardUpdateInput/DashboardUpdateInput.js
+++ src/components/DashboardUpdateInput/DashboardUpdateInput.js
@@ -72,21 +72,21 @@ class DashboardUpdateInput extends Component {
   render() {
     return (
         <form onSubmit={this.saveUpdate}>
-                <input placeholder="batch" value={this.state.enteredbatch} name="batch"onChange={this.changeHandle}/>
-                <input placeholder="name" value={this.state.enteredname} name="name"onChange={this.changeHandle}/>
-                <input placeholder="product_line" value={this.state.enteredproduct_line} name="product_line"onChange={this.changeHandle}/>
-                <input placeholder="length" value={this.state.enteredlength} name="length"onChange={this.changeHandle}/>
-                <input placeholder="texture" value={this.state.enteredtexture} name="texture"onChange={this.changeHandle}/>
-                <input placeholder="color" value={this.state.enteredcolor} name="color"onChange={this.changeHandle}/>
-                <input placeholder="hair_type" value={this.state.enteredhair_type} name="hair_type"onChange={this.changeHandle}/>
-                <input placeholder="region_type" value={this.state.enteredregion_type} name="region_type"onChange={this.changeHandle}/>
-                <input placeholder="factory" value={this.state.enteredfactory} name="factory"onChange={this.changeHandle}/>
-                <input placeholder="current_location" value={this.state.enteredcurrent_location} name="current_location"onChange={this.changeHandle}/>
-                <input placeholder="quantity" value={this.state.enteredquantity} name="quantity"onChange={this.changeHandle}/>
-                <input placeholder="cost_of_batch" value={this.state.enteredcost_of_batch} name="cost_of_batch"onChange={this.changeHandle}/>
-                <input placeholder="price_per_unit" value={this.state.enteredprice_per_unit} name="price_per_unit"onChange={this.changeHandle}/>
-                <input placeholder="photos" value={this.state.enteredphotos} name="photos"onChange={this.changeHandle}/>
-                <input placeholder="qr_code" value={this.state.enteredqr_code} name="qr_code"onChange={this.changeHandle}/>
+                <input type="text" placeholder="batch" value={this.state.enteredbatch} name="batch"onChange={this.changeHandle}/>
+                <input type="text" placeholder="name" value={this.state.enteredname} name="name"onChange={this.changeHandle}/>
+                <input type="text" placeholder="product_line" value={this.state.enteredproduct_line} name="product_line"onChange={this.changeHandle}/>
+                <input type="number" placeholder="length" value={this.state.enteredlength} name="length"onChange={this.changeHandle}/>
+                <input type="text" placeholder="texture" value={this.state.enteredtexture} name="texture"onChange={this.changeHandle}/>
+                <input type="text" placeholder="color" value={this.state.enteredcolor} name="color"onChange={this.changeHandle}/>
+                <input type="text" placeholder="hair_type" value={this.state.enteredhair_type} name="hair_type"onChange={this.changeHandle}/>
+                <input type="text" placeholder="region_type" value={this.state.enteredregion_type} name="region_type"onChange={this.changeHandle}/>
+                <input type="text" placeholder="factory" value={this.state.enteredfactory} name="factory"onChange={this.changeHandle}/>
+                <input type="text" placeholder="current_location" value={this.state.enteredcurrent_location} name="current_location"onChange={this.changeHandle}/>
+                <input type="number" placeholder="quantity" value={this.state.enteredquantity} name="quantity"onChange={this.changeHandle}/>
+                <input type="number" placeholder="cost_of_batch" value={this.state.enteredcost_of_batch} name="cost_of_batch"onChange={this.changeHandle}/>
+                <input type="number" placeholder="price_per_unit" value={this.state.enteredprice_per_unit} name="price_per_unit"onChange={this.changeHandle}/>
+                <input type="text" placeholder="photos" value={this.state.enteredphotos} name="photos"onChange={this.changeHandle}/>
+                <input type="text"placeholder="qr_code" value={this.state.enteredqr_code} name="qr_code"onChange={this.changeHandle}/>
                 <button className="saveupdatebtn">Save update</button>
         </form>
     );
