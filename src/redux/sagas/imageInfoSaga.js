1aee18dd03e611974671798cf3f728b890486ead
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-06-16 21:37:23 -0500

    last commit to temp branch

--- src/redux/sagas/imageInfoSaga.js
+++ src/redux/sagas/imageInfoSaga.js
@@ -3,22 +3,22 @@ import { put, takeLatest } from 'redux-saga/effects';
 
 // worker Saga: will be fired on "FETCH_USER" actions
 function* postImageUrl() {
-  // try {
-  //   const config = {
-  //     headers: { 'Content-Type': 'application/json' },
-  //     withCredentials: true,
-  //   };
+  try {
+    const config = {
+      headers: { 'Content-Type': 'application/json' },
+      withCredentials: true,
+    };
 
-  //       // const data = {
-  //       //     imageUrl: action.payload
-  //       // }
+        // const data = {
+        //     imageUrl: action.payload
+        // }
 
-  //       const response = yield axios.post('api/imageurl', data, config);
-  //       console.log(response);
-  //       // AFTER POST COMES BACK SUCCESSFUL, DO A GET!
-  // } catch (error) {
-  //   console.log('Image URL post failed: ', error);
-  // }
+        const response = yield axios.post('api/imageurl', data, config);
+        console.log(response);
+        // AFTER POST COMES BACK SUCCESSFUL, DO A GET!
+  } catch (error) {
+    console.log('Image URL post failed: ', error);
+  }
 }
 
 function* imageInfoSaga() {
